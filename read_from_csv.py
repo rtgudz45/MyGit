d732138cdbb7803028007ee10ae5b9365f057aba
anshuman biswal <anshuman@shopsocially.com>
2019-09-18 00:10:21 +0530

    Read emails from an input csv

--- read_from_csv.py
+++ read_from_csv.py
@@ -1,16 +1,36 @@
+import os
 import csv
 import requests
 import argparse
 import logging
 
-logging.basicConfig(level = logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S',level = logging.DEBUG)
 a = 'sasa'
 
+
+def process_emails(args):
+
+        if not os.path.isfile(args.input_file):
+                logging.info('No input file provided!!..Exiting...')
+                return
+
+        with open(args.input_file,'rb') as input_file:
+                rows = csv.DictReader(input_file)
+                for row in rows:
+                        logging.info('Email is %s'%row.get('EMAIL'))
+
+
+
+
+
 def main():
-        print 'Hello main , '+a
+        logging.info('Inside main....Wakanda forever!!')
+        parser = argparse.ArgumentParser(description='Script to read emails from a csv file and process them')
+        parser.add_argument( '-i', '--input_file',help='input csv file having emails')
 
-        logging.info('The value of a is %s'%a)
+        args = parser.parse_args()
 
+        process_emails(args)
         
 
 if __name__ == '__main__':
